899d2145fa09b476c3c87a94fe80b12c0717096d
maksim.balabash <maksim.balabash@greyloud.com>
2017-10-17 11:14:47 +0300

    add plugin

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const through = require('through2');
+const PluginError = require('gulp-util').PluginError;
+const gutil = require('gulp-util');
+const sqip = require('sqip');
+const PLUGIN_NAME = 'gulp-sqip';
+
+module.exports = () => {
+    /**
+     * @this {Transform}
+     */
+
+    const placeholders = [];
+
+    const transform = function(file, encoding, callback) {
+        if (file.isNull()) return callback(null, file);
+        if (file.isStream()) this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+        if (file.isBuffer()) {
+            const svg = sqip({
+                filename: file.history[0],
+                numberOfPrimitives: 22
+            });
+            svg.path = getFilenameFromPath(file.history[0]) + '.svg';
+            placeholders.push(svg);
+            callback();
+        }
+    };
+
+    const endStreem = function(callback) {
+        placeholders.forEach(svg => {
+            this.push(new gutil.File({
+                path: svg.path,
+                contents: new Buffer(String(svg.final_svg))
+            }));
+        });
+        callback();
+    };
+
+    const getFilenameFromPath = (path) => {
+        return path.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, "");
+    };
+
+    return through.obj(transform, endStreem);
+};
